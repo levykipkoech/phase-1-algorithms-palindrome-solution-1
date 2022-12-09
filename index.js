7f6bb8d25927c2364d5d2b68484a2cb78312370d
levy kipkoech <kipkoechlevy096@gmail.com>
2022-12-09 16:06:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+ 
 function isPalindrome(word) {
-  // Write your algorithm here
+  let revesred = word.split("").reverse().join("")
+    if (revesred === word)
+    return true;
+    return false;
+
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+ 
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
